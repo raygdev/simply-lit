48a2445677542d147b6c3ff2ffc6be44a0e12e04
Steve <scampbellatc@gmail.com>
2023-05-17 18:05:47 -0400

    updated Schema

--- server/models/Letters.js
+++ server/models/Letters.js
@@ -2,19 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const letterSchema = new Schema({
-    size: Number,
-    type: String,
-    font: String,
+    size: { type: String, required: true },
+    type: { type: String, required: true },
+    font: { type: String, required: true },
     letters: {
         type: [{
-            type: String,
-            symb: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', "!", "#", "$", "&", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ]
-        }],
-        required: true,
-        quantity: {
+            sym: {type: String, required: true},
             totalStock: Number,
             numAvailable: Number
-        }
+        }],
+        required: true
 
     },
     
