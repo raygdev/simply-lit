e4c6c64d117f3800fcb592947a8c14a5648fd63e
raygdev <101662915+raygdev@users.noreply.github.com>
2023-05-17 22:32:53 -0400

    Fix: add leading forward slashes to routes

--- server/routes/lettersRoutes/lettersRoutes.js
+++ server/routes/lettersRoutes/lettersRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const lettersControllers = require("../../controllers/lettersControllers/lettersControllers")
 
 router.get("/letters", lettersControllers.getAllLetters)
-router.get("letters/:id", lettersControllers.getLettersById)
-router.put("letters/:id", lettersControllers.updateLetters)
-router.delete("letters/:id", lettersControllers.deleteById)
-router.post("letters", lettersControllers.createLetters)
+router.get("/letters/:id", lettersControllers.getLettersById)
+router.put("/letters/:id", lettersControllers.updateLetters)
+router.delete("/letters/:id", lettersControllers.deleteById)
+router.post("/letters", lettersControllers.createLetters)
 
 module.exports = router
