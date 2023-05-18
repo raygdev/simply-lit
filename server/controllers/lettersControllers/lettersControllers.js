78fa50b98b492a8f98bb54651935a1cfbadc726f
raygdev <101662915+raygdev@users.noreply.github.com>
2023-05-17 21:34:54 -0400

    Add boilerplate routes and controllers
    Add controllers directory
    
    ad controllers directory and lettersControllers directory
    
    add lettersControllers.js to set up route controllers
    for letters routes.
    
    Add lettersRoutes directory
    
    add lettersRoutes directory and lettersRoutes.js
    to use lettersControllers
    
    Setup boilerplate for letters controllers
    
    Add comments to controllers
    
    add comments to identify which controller belongs
    to which route
    
    Add routes and controllers for letters

---
+++ server/controllers/lettersControllers/lettersControllers.js
@@ -0,0 +1,75 @@
+/**
+ * GET /letters
+ * 
+ * responds with json array of all letters with type and size
+ * will provide filters with queries to this route
+ */
+
+exports.getAllLetters = async (req, res) => {
+    try{
+
+    } catch (e) {
+
+    }
+}
+
+/**
+ * GET /letters/:id
+ * 
+ * responds with json for the specific type and
+ * size based on the id.
+ */
+
+exports.getLettersById = async (req, res) => {
+    try {
+
+    } catch (e) {
+
+    }
+}
+
+/**
+ * POST /letters
+ * 
+ * will create the letters model, and respond
+ * with json with the newly created letters model
+ */
+
+exports.createLetters = async (req, res) => {
+    try {
+
+    } catch (e) {
+
+    }
+}
+
+/**
+ * PUT /letters/:id
+ * 
+ * will updated the inventory of letters
+ * to add or subtract from totalStock and
+ * numberAvailable
+ */
+
+exports.updateLetters = async (req, res) => {
+    try {
+
+    } catch (e) {
+
+    }
+}
+
+/**
+ * DELETE /letters/:id
+ * 
+ * in case we want to delete a specifi size and
+ * type all together.
+ */
+
+exports.deleteById = async (req, res) => {
+    try {
+
+    } catch (e) {
+
+    }
+}
