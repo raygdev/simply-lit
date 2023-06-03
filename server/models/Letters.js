3bdaaac59cae11d60c9860bdc88bf54df3806dd7
raygdev <101662915+raygdev@users.noreply.github.com>
2023-06-03 09:37:47 -0400

    Add defaults for schema
    Add defaults
    
    adds defaults to letters array
    and provides defaults for stock
    
    Exports a default Array
    
    exports a default letters array for
    the schema
    
    Remove console logs
    
    removes console logs and fixes
    indentations

--- server/models/Letters.js
+++ server/models/Letters.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const { symbols } = require("./utils/letters.utils")
+
 const letterSchema = new Schema({
     size: { type: String, required: true },
     type: { type: String, required: true },
     font: { type: String, required: true },
     letters: {
         type: [{
-            sym: {type: String, required: true},
-            totalStock: Number,
-            numAvailable: Number
+            sym: { type: String, required: true },
+            totalStock: { type: Number, default: 0 },
+            numAvailable: { type: Number, default: 0 }
         }],
-        required: true
+        required: true,
+        default: symbols
 
     },
     
