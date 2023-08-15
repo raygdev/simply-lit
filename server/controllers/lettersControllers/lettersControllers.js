7893137f0363c217787c805b10c212cf22df2dd3
raygdev <101662915+raygdev@users.noreply.github.com>
2023-08-15 13:25:08 -0400

    Fix update controller and refactor
    Fixes the update controller to accept an array
    with the modified stock values.
    
    provides better error handling, chaining
    a catch to each schema function.
    
    return null if the find and update errors
    allowing the not found status to run.

--- server/controllers/lettersControllers/lettersControllers.js
+++ server/controllers/lettersControllers/lettersControllers.js
@@ -1,116 +1,147 @@
 /**
  * GET /letters
- * 
+ *
  * responds with json array of all letters with type and size
  * will provide filters with queries to this route
  */
 
 const { Letters } = require("../../models/Letters.js")
 
-
 exports.getAllLetters = async (req, res) => {
-    try{
-        const filter = {}
-        
-        if(req.query.size){
+  try {
+    const filter = {}
 
-            filter.size = req.query.size
-        }
-
-        if(req.query.type){
-            filter.type = req.query.type
-        }
-
-        if(req.query.font){
-            filter.font = req.query.font
-        }
-
-        const letters = await Letters.find(filter).exec()
-
-    
-        res.json(letters)
-
-    } catch (e) {
-        res.status(500).json({error: `There was an error: ${e}`})
+    if (req.query.size) {
+      filter.size = req.query.size
     }
+
+    if (req.query.type) {
+      filter.type = req.query.type
+    }
+
+    if (req.query.font) {
+      filter.font = req.query.font
+    }
+
+    const letters = await Letters.find(filter).exec()
+
+    res.json(letters)
+  } catch (e) {
+    res.status(500).json({ error: `There was an error: ${e}` })
+  }
 }
 
 /**
  * GET /letters/:id
- * 
+ *
  * responds with json for the specific type and
  * size based on the id.
  */
 
 exports.getLettersById = async (req, res) => {
-    try {
-        const letterId = req.params.id
-        const letters = await Letters.findById(letterId).exec()
-        res.json(letters)
-    } catch (e) {
-        res.status(500).json({error: `There was an error: ${e}`})
-    }
+  try {
+    const letterId = req.params.id
+    const letters = await Letters.findById(letterId).exec()
+    res.json(letters)
+  } catch (e) {
+    res.status(500).json({ error: `There was an error: ${e}` })
+  }
 }
 
 /**
  * POST /letters
- * 
+ *
  * will create the letters model, and respond
  * with json with the newly created letters model
  */
 
 exports.createLetters = async (req, res) => {
-    try {
-        const { size, type, font, letters } = req.body
-        const newLetter = new Letters({ size, type, font, letters })
-        await newLetter.save()
+  try {
+    const { size, type, font, letters } = req.body
+    const newLetter = new Letters({ size, type, font, letters })
+    await newLetter.save()
 
-        res.status(201).json(newLetter)
-    } catch (e) {
-        res.status(501).json(`${e}: Internal server error`)
-    }
+    res.status(201).json(newLetter)
+  } catch (e) {
+    res.status(501).json(`${e}: Internal server error`)
+  }
 }
 
 /**
  * PUT /letters/:id
- * 
+ *
  * will updated the inventory of letters
  * to add or subtract from totalStock and
  * numberAvailable
  */
 
 exports.updateLetters = async (req, res) => {
-    try {
-        const { id } = req.params
-        const { quantity } = req.body
+  try {
+    const { id } = req.params
+    const { updateItems } = req.body
+    console.log(id)
 
-        const letter = await Letters.findById(id)
-
-        if(!letter){
-            return res.status(404).json({ error:'Letter not found'})
-        }
-        // These will be adjusted later to not have them hard coded as an increment
-        // as they may also be decremented
-        letter.totalStock += quantity
-        letter.numberAvailable += quantity
-
-        await letter.save()
-    } catch (e) {
-        res.status(500).json({e: 'Internal Server Error'})
+    if (!updateItems) {
+      res.status(422).json({
+        errorMessage:
+          `Expected an updateItems property in the body and got ${updateItems} instead.`
+      })
+      return
     }
+    let letterError
+    const letter = await Letters.findByIdAndUpdate(
+      { _id: id },
+      { letters: updateItems },
+      { returnDocument: "after" }
+    ).catch(e => {
+      console.log(e)
+      letterError = {
+        errorName: e.name,
+        errorPath: e.path,
+        errorMessage: e.message,
+        errorReason: e.reason
+      }
+      return null
+    })
+
+    if (!letter) {
+      return res.status(404).json({
+        error: "Letter not found",
+        letterError
+      })
+    }
+
+    await letter.save().catch(e => {
+      console.log(e)
+      return res.status(400).json({
+        message: "Something went wrong saving and updating the items.",
+        errorName: e.name,
+        errorMessage: e.message,
+        errorPath: e.path,
+        errorReason: e.reason
+      })
+    })
+    res.status(200).json({
+      message: "Updated successfully",
+      letters: letter.toJSON()
+    })
+  } catch (e) {
+    res.status(500).json({
+      error: "Internal Server Error",
+      errorMessage: e.message,
+      errorStack: e.stack
+    })
+  }
 }
 
 /**
  * DELETE /letters/:id
- * 
+ *
  * in case we want to delete a specifi size and
  * type all together.
  */
 
 exports.deleteById = async (req, res) => {
-    try {
-       
-    } catch (e) {
-        
-    }
+  try {
+  } catch (e) {}
 }
