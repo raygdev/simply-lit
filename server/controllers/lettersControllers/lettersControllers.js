ff44472d418a557eefef6502b700629c03e8ea98
raygdev <101662915+raygdev@users.noreply.github.com>
2023-08-15 21:32:51 -0400

    Add some helpful comments
    adds helpful comments and sets some reminders
    for later development

--- server/controllers/lettersControllers/lettersControllers.js
+++ server/controllers/lettersControllers/lettersControllers.js
@@ -23,6 +23,7 @@ exports.getAllLetters = async (req, res) => {
       filter.font = req.query.font
     }
 
+    //TODO: provide better error handling with catch
     const letters = await Letters.find(filter).exec()
 
     res.json(letters)
@@ -41,6 +42,7 @@ exports.getAllLetters = async (req, res) => {
 exports.getLettersById = async (req, res) => {
   try {
     const letterId = req.params.id
+    //TODO: provide catch here for better error handling
     const letters = await Letters.findById(letterId).exec()
     res.json(letters)
   } catch (e) {
@@ -57,8 +59,14 @@ exports.getLettersById = async (req, res) => {
 
 exports.createLetters = async (req, res) => {
   try {
+    //letter may be undefined in the body... a default is set
     const { size, type, font, letters } = req.body
+    /**
+     * more checks should be made to ensure that
+     * size, type, and font are defined.
+     */
     const newLetter = new Letters({ size, type, font, letters })
+    //TODO: provide a catch for better error messages if saving fails
     await newLetter.save()
 
     res.status(201).json(newLetter)
@@ -81,32 +89,59 @@ exports.updateLetters = async (req, res) => {
     const { updateItems } = req.body
     console.log(id)
 
+    //update items should be defined in the body...
+    //may need some more strict checks, but for now this will do.
     if (!updateItems) {
       res.status(422).json({
-        errorMessage:
-          `Expected an updateItems property in the body and got ${updateItems} instead.`
+        errorMessage: `Expected an updateItems property in the body and got ${updateItems} instead.`
       })
       return
     }
     let letterError
     const letter = await Letters.findByIdAndUpdate(
+      //find by id
       { _id: id },
+      //letter will be updated with update items
       { letters: updateItems },
+      /**
+       * tells mongoose to return the updated document and
+       * not the old one.
+       */
       { returnDocument: "after" }
-    ).catch(e => {
-      console.log(e)
-      letterError = {
-        errorName: e.name,
-        errorPath: e.path,
-        errorMessage: e.message,
-        errorReason: e.reason
-      }
-      return null
-    })
+    )
+      .exec()
+      //run a catch for more specific error handling
+      .catch(e => {
+        console.log(e)
+        letterError = {
+          errorName: e.name,
+          errorPath: e.path,
+          errorMessage: e.message,
+          errorReason: e.reason
+        }
+        /**
+         * Coerce to null so that letter becomes falsy in the
+         * event of some cast error from mongoose
+         *
+         * letter error will be assigned the value of the error and sent
+         * with the appropriate status code.
+         *
+         * The error will typically be a cast error in the event that
+         * the id cannot be cast to an object id (occurs behind the scenes) from mongoose.
+         *
+         * if an otherwise valid id is passed, letter will still be null,
+         * but no error will occur and this catch will never get called.
+         */
+        return null
+      })
 
     if (!letter) {
       return res.status(404).json({
         error: "Letter not found",
+        /**
+         * if letter error is defined, both errors will show
+         * if it is not only the hard-coded error will display
+         */
         letterError
       })
     }
