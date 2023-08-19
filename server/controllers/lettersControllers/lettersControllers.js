6016a21cb87790bb7a19b9e6576cc11c9a5bba12
raygdev <101662915+raygdev@users.noreply.github.com>
2023-08-19 19:52:04 -0400

    Remove comments

--- server/controllers/lettersControllers/lettersControllers.js
+++ server/controllers/lettersControllers/lettersControllers.js
@@ -23,7 +23,6 @@ exports.getAllLetters = async (req, res) => {
       filter.font = req.query.font
     }
 
-    //TODO: provide better error handling with catch
     const letters = await Letters.find(filter).exec()
 
     res.json(letters)
@@ -42,7 +41,6 @@ exports.getAllLetters = async (req, res) => {
 exports.getLettersById = async (req, res) => {
   try {
     const letterId = req.params.id
-    //TODO: provide catch here for better error handling
     const letters = await Letters.findById(letterId).exec()
     res.json(letters)
   } catch (e) {
@@ -59,14 +57,8 @@ exports.getLettersById = async (req, res) => {
 
 exports.createLetters = async (req, res) => {
   try {
-    //letter may be undefined in the body... a default is set
     const { size, type, font, letters } = req.body
-    /**
-     * more checks should be made to ensure that
-     * size, type, and font are defined.
-     */
     const newLetter = new Letters({ size, type, font, letters })
-    //TODO: provide a catch for better error messages if saving fails
     await newLetter.save()
 
     res.status(201).json(newLetter)
@@ -78,9 +70,9 @@ exports.createLetters = async (req, res) => {
 /**
  * PUT /letters/:id
  *
- * will updated the inventory of letters
- * to add or subtract from totalStock and
- * numberAvailable
+ * Accepts the entire array of leters
+ * Updates occur on the property in 
+ * the frontend
  */
 
 exports.updateLetters = async (req, res) => {
@@ -88,9 +80,6 @@ exports.updateLetters = async (req, res) => {
     const { id } = req.params
     const { updateItems } = req.body
     console.log(id)
-
-    //update items should be defined in the body...
-    //may need some more strict checks, but for now this will do.
     if (!updateItems) {
       res.status(422).json({
         errorMessage: `Expected an updateItems property in the body and got ${updateItems} instead.`
@@ -99,18 +88,11 @@ exports.updateLetters = async (req, res) => {
     }
     let letterError
     const letter = await Letters.findByIdAndUpdate(
-      //find by id
       { _id: id },
-      //letter will be updated with update items
       { letters: updateItems },
-      /**
-       * tells mongoose to return the updated document and
-       * not the old one.
-       */
       { returnDocument: "after" }
     )
       .exec()
-      //run a catch for more specific error handling
       .catch(e => {
         console.log(e)
         letterError = {
@@ -119,29 +101,12 @@ exports.updateLetters = async (req, res) => {
           errorMessage: e.message,
           errorReason: e.reason
         }
-        /**
-         * Coerce to null so that letter becomes falsy in the
-         * event of some cast error from mongoose
-         *
-         * letter error will be assigned the value of the error and sent
-         * with the appropriate status code.
-         *
-         * The error will typically be a cast error in the event that
-         * the id cannot be cast to an object id (occurs behind the scenes) from mongoose.
-         *
-         * if an otherwise valid id is passed, letter will still be null,
-         * but no error will occur and this catch will never get called.
-         */
         return null
       })
 
     if (!letter) {
       return res.status(404).json({
         error: "Letter not found",
-        /**
-         * if letter error is defined, both errors will show
-         * if it is not only the hard-coded error will display
-         */
         letterError
       })
     }
