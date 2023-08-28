3fedb58ab94600bcb1a79d0a70be052c9ecaf8e1
raygdev <101662915+raygdev@users.noreply.github.com>
2023-08-28 19:37:15 -0400

    Added proxy to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/letters': {
+        target: "http://127.0.0.1:8080",
+        cors: true,
+        changeOrigin: true,
+        secure: false,
+        ws:true,
+      },
+    }
+  }
 })
