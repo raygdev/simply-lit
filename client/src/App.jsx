28d956ccd7ca853b86c3981cb8459dd898bac326
Steve <scampbellatc@gmail.com>
2023-08-28 13:01:15 -0400

    updated files, new error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
 import { React, useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from '../src/pages/Home'
 import About from '../src/pages/About'
 import Login from '../src/pages/Login'
 import './App.css'
-import Layout from './pages/Layout'
+import Layout from '../src/pages/Layout'
 
-function App() {
+export default function App() {
   
 
   return (
-    <BrowserRouter>
+    
       <Routes>
-        <Route element={<Layout />}>
+        <Route path='/' element={<Layout />}>
 
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/login' element={<Login />} />
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='login' element={<Login />} />
 
+        
         </Route>
 
       </Routes>
-    </BrowserRouter>
+    
   )
 }
 
-export default App
+
