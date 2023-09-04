b4069d6fee8c6288468f8f53badd18b419919fa4
raygdev <101662915+raygdev@users.noreply.github.com>
2023-09-04 14:30:28 -0400

    Use HeaderLinks
    uses header links and a logo placeholder

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import HeaderLinks from '../components/HeaderLinks'
 
 export default function Header(){
     return (
-        <header>
-            <Link to="/">This is the header</Link>
+        <header 
+          className=
+          ' z-[2] bg-[#fefefe] drop-shadow-lg flex justify-between items-center p-2 sticky top-0 text-orange-400'
+        >
+            <div>Logo</div>
+            <HeaderLinks />
         </header>
     )
 }
