7bceb331a19ceea5db6bd83ad9a3b66fe18af055
Steve <scampbellatc@gmail.com>
2023-10-16 06:30:38 -0400

    updated log in form style

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,22 +20,35 @@ export default function Login(){
    const errorMessage = useActionData()
     return (
         <>
-        <h1>this is the login page</h1>
+        
         {errorMessage && <h3>{errorMessage}</h3>}
-        <Form method="post" replace onSubmit={(e) => {submit(e.currentTarget)}}>
+        <Form 
+        className="flex flex-col justify-center content-center mt-8 max-w-md m-auto border-2 border-black rounded p-6" 
+        method="post" 
+        replace 
+        onSubmit={(e) => {submit(e.currentTarget)}}>
+            <h3 className='self-center mb-4'>Please log in to your account</h3>
+            <label className='' htmlFor='myEmail'>Email</label>
             <input 
-            type="email"
-            name="email"
-            placeholder="Email address" 
+            id='myEmail'
+            className='text-2xl border-2 rounded p-1 mb-4'
+            type='email'
+            name='email'
+            placeholder="your@company.com" 
+            />
+            <label htmlFor='myPassword'>Password</label>
+            <input 
+            id='myPassword'
+            className='mb-8 border-2 rounded p-1 text-2xl '
+            type='password'
+            name='password'
+            placeholder='Password'
+            
             />
 
-            <input 
-            type="password" 
-            name="password"
-            placeholder="Password"
-            />
-
-            <button type="submit">Log In</button>
+            <button
+            className='bg-gray-400 py-1.5 px-2.5 rounded'
+            >Log In</button>
 
         </Form>
         </>
