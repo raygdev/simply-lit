80638c0d4eaf8c75ae7193ba4dfc62964766c3e4
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-05 10:19:44 -0500

    Add login route with user

--- server/routes/userRoutes/userRoutes.js
+++ server/routes/userRoutes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const userControllers = require("../../controllers/userControllers/userControllers")
+const login = require("../../controllers/loginController")
+const { isAuth } = require("../../middleware/auth")
 
 router.post("/user", userControllers.createUser)
+router.post("/login", login.loginController)
 
 module.exports = router
