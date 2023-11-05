c16587a451ec905bfec18f45b5a35e02e3aed2f8
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-05 10:20:07 -0500

    Add get route for single user

--- server/routes/userRoutes/userRoutes.js
+++ server/routes/userRoutes/userRoutes.js
@@ -5,6 +5,7 @@ const login = require("../../controllers/loginController")
 const { isAuth } = require("../../middleware/auth")
 
 router.post("/user", userControllers.createUser)
+router.get("/user/:id", isAuth, userControllers.getUserById)
 router.post("/login", login.loginController)
 
 module.exports = router
