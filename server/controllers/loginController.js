b67c27422ebe792d364ba949656dae0ff4fc90f7
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-05 10:15:35 -0500

    Add login controller
    adds a login controller and this
    controller is responsible for setting cookies

---
+++ server/controllers/loginController.js
@@ -0,0 +1,47 @@
+const { User } = require("../models/User")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+exports.loginController = async (req, res) => {
+    const { email, password } = req.body
+    if(!email || !password) return res.status(409).json({
+        success:false,
+        message: "email and password fields required"
+    })
+
+    try{
+        const found = await User.findOne({ email })
+        const user = found.toJSON()
+        if(!found) return res.status(404).json({
+            success:false,
+            message: "Could not find that user, please check your credentials"
+        })
+
+        const pass = await bcrypt.compare(password, user.password)
+
+        if(pass) {
+            delete user.password
+            res.cookie("token", jwt.sign(user, process.env.JWT_SECRET), {
+                secure: false,
+                httpOnly: true,
+                maxAge: 1000 * 60 * 5
+            }). status(200).json({
+                success: true,
+                message: "User successfully logged in",
+                user
+            })
+        } else {
+            res.status(404).json({
+                success:false,
+                message: "Could not find that user, please check your credentials"
+            })
+        }
+    }
+    catch(e) {
+        console.log(e)
+        res.status(400).json({
+            success:false,
+            message: "Something went wrong"
+        })
+    }
+}
