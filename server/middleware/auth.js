35fd9d5ab74f5a8e804e432ddadf5f8ee0b34906
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-05 08:19:35 -0500

    Create auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+const { User } = require("../models/User")
+
+async function isAuth(req,res,next) {
+    const token = req.cookie.token
+    if(!token) return res.status(401).json({
+        success:false,
+        message: "Not authorized, please log in",
+    })
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById(decoded)
+        req.user = user
+        next()
+
+    }
+    catch(e) {
+        console.log(e)
+        res.status(401).json({
+            success:false,
+            message: "Something went wrong"
+        })
+    }
+
+
+}
+
+module.exports = { isAuth }
