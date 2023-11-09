a9e68c35a601ad56977c2cc50caa4f20f457092e
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-08 22:10:54 -0500

    Add auth middleware
    adds auth middleware to the required routes
    to be protected

--- server/routes/lettersRoutes/lettersRoutes.js
+++ server/routes/lettersRoutes/lettersRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const lettersControllers = require("../../controllers/lettersControllers/lettersControllers")
+const { isAuth } = require("../../middleware/auth")
 
-router.get("/letters", lettersControllers.getAllLetters)
-router.get("/letters/:id", lettersControllers.getLettersById)
-router.put("/letters/:id", lettersControllers.updateLetters)
-router.delete("/letters/:id", lettersControllers.deleteById)
-router.post("/letters", lettersControllers.createLetters)
+router.get("/letters", isAuth, lettersControllers.getAllLetters)
+router.get("/letters/:id", isAuth, lettersControllers.getLettersById)
+router.put("/letters/:id", isAuth, lettersControllers.updateLetters)
+router.delete("/letters/:id", isAuth, lettersControllers.deleteById)
+router.post("/letters", isAuth, lettersControllers.createLetters)
 
 module.exports = router
